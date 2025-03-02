798f921473a57f64a702eb0add6c2b0cd8f4e36e
ankitpatil3003 <apatil@binghamton.edu>
2025-03-02 07:28:43 -0500

    loading the LLM

--- src/langgraphagenticai/main.py
+++ src/langgraphagenticai/main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import json
 from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
+from src.langgraphagenticai.LLMS.groqllm import GroqLLM
 
+# MAIN Function START
 def load_langgraph_agenticai_app():
     """
     Loads and runs the LangGraph AgenticAI application with Streamlit UI.
@@ -17,3 +19,27 @@ def load_langgraph_agenticai_app():
     if not user_input:
         st.error("Error: Failed to load user input from the UI.")
         return
+    
+        # Text input for user message
+    if st.session_state.IsFetchButtonClicked:
+        user_message = st.session_state.timeframe 
+    else :
+        user_message = st.chat_input("Enter your message:")
+
+    if user_message:
+        try:
+            # Configure LLM
+            obj_llm_config = GroqLLM(user_controls_input=user_input)
+            model = obj_llm_config.get_llm_model()
+            
+            if not model:
+                st.error("Error: LLM model could not be initialized.")
+                return
+
+            # Initialize and set up the graph based on use case
+            usecase = user_input.get('selected_usecase')
+            if not usecase:
+                st.error("Error: No use case selected.")
+                return
+        except Exception as e:
+                raise ValueError(f"Error Occurred with Exception : {e}")
