a9a396a5e870f4d96741b7f9d8f54fe4f99c1b31
ankitpatil3003 <apatil@binghamton.edu>
2025-03-02 07:34:50 -0500

    graph builder complete

--- src/langgraphagenticai/main.py
+++ src/langgraphagenticai/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import json
 from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
 from src.langgraphagenticai.LLMS.groqllm import GroqLLM
+from src.langgraphagenticai.graph.graph_builder import GraphBuilder
 
 # MAIN Function START
 def load_langgraph_agenticai_app():
@@ -19,27 +20,37 @@ def load_langgraph_agenticai_app():
     if not user_input:
         st.error("Error: Failed to load user input from the UI.")
         return
-    
-        # Text input for user message
+
+    # Text input for user message
     if st.session_state.IsFetchButtonClicked:
         user_message = st.session_state.timeframe 
     else :
         user_message = st.chat_input("Enter your message:")
 
     if user_message:
-        try:
-            # Configure LLM
-            obj_llm_config = GroqLLM(user_controls_input=user_input)
-            model = obj_llm_config.get_llm_model()
-            
-            if not model:
-                st.error("Error: LLM model could not be initialized.")
-                return
+            try:
+                # Configure LLM
+                obj_llm_config = GroqLLM(user_controls_input=user_input)
+                model = obj_llm_config.get_llm_model()
+                
+                if not model:
+                    st.error("Error: LLM model could not be initialized.")
+                    return
 
-            # Initialize and set up the graph based on use case
-            usecase = user_input.get('selected_usecase')
-            if not usecase:
-                st.error("Error: No use case selected.")
-                return
-        except Exception as e:
-                raise ValueError(f"Error Occurred with Exception : {e}")
+                # Initialize and set up the graph based on use case
+                usecase = user_input.get('selected_usecase')
+                if not usecase:
+                    st.error("Error: No use case selected.")
+                    return
+                
+
+                ### Graph Builder
+                graph_builder=GraphBuilder(model)
+                try:
+                    graph = graph_builder.setup_graph(usecase)
+                except Exception as e:
+                    st.error(f"Error: Graph setup failed - {e}")
+                    return
+
+            except Exception as e:
+                 raise ValueError(f"Error Occurred with Exception : {e}")
